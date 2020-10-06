f2697d64ff26005e18ee1d9198d413f91a34b075
dasecure <vincent@dasecure.com>
2020-10-05 20:17:35 -0700

    Changed flask output text

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "My name is Vincent.."
+    return "Hello beautiful World!!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
